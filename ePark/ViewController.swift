7e4fcf1d467cceaafdaa7b6d11ee0ae40c29a5a1
hasanguzelmansur <hasan.guzelmansur@gmail.com>
2021-05-12 17:13:16 +0300

    Kullanıcı oluşturma işlemi eklendi ve View Controller'lar oluşturuldu.

--- ePark/ViewController.swift
+++ ePark/ViewController.swift
@@ -6,14 +6,60 @@
 //
 
 import UIKit
+import Firebase
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var eMailText: UITextField!
+    
+    @IBOutlet weak var passwordText: UITextField!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func signInClicked(_ sender: Any) {
+        performSegue(withIdentifier: "toMap", sender: nil)
+    }
+    
+    
+    @IBAction func signUpClicked(_ sender: Any) {
+        
+        if eMailText.text != "" && passwordText.text != "" {
+            
+            Auth.auth().createUser(withEmail: eMailText.text!, password: passwordText.text!) { (authdata, error) in
+                if error != nil {
+                    
+                    self.makeAlert(titleIn: "HATA!!", messageIn: error?.localizedDescription ?? "Error")
+                    
+                }else{
+                    self.performSegue(withIdentifier: "toMap", sender: nil)
+                }
+            }
+            
+            
+        }else{
+            
+            makeAlert(titleIn: "HATA!!", messageIn: "eMail ve şifre boş olamaz!!")
+            
+        }
+        
+        
+        
+    }
+    
+    func makeAlert(titleIn:String, messageIn:String){
+        
+        let alert = UIAlertController(title: titleIn, message: messageIn, preferredStyle: UIAlertController.Style.alert)
+        
+        let okButton = UIAlertAction(title: "Tamam", style: UIAlertAction.Style.default, handler:   nil)
+        
+        alert.addAction(okButton)
+        self.present(alert, animated: true, completion: nil)
+        
+    }
+    
 }
 
